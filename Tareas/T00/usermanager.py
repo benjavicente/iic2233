75fa412049a953a0e99e6043f266edbf32b632b2
Benjamín Vicente <benjavicente@uc.cl>
2020-03-14 23:43:26 -0300

    Creación de dejar_de_seguir() en Usuario :truck:

--- Tareas/T00/usermanager.py
+++ Tareas/T00/usermanager.py
@@ -1,26 +1,34 @@
 """
 Libreria para administrar datos relacionados con el usuario
+Incluye:
+    class Usuario
+    set usuarios
+    ------------
+    str carpeta_datos
+    str path_usuarios
+    str path_seguidores
 """
+
+# TODO simplificar la manera en la que leo  y escribo los archivos
 
 from os import path
 
 
-CARPETA_DATOS = "data"
+carpeta_datos = "data"
 
-PATH_USUARIOS = path.join(CARPETA_DATOS, "usuarios.csv")
-PATH_SEGUIDORES = path.join(CARPETA_DATOS, "seguidores.csv")
-
-lista_usuarios = list()
+path_usuarios = path.join(carpeta_datos, "usuarios.csv")
+path_seguidores = path.join(carpeta_datos, "seguidores.csv")
 
 
 class Usuario:
     def __init__(self, nombre):
         self.nombre = nombre
-        # agrego a la lista automaticamente
-        lista_usuarios.append(self)
 
     def __str__(self):
         return self.nombre
+
+    def __repr__(self):
+        return str(self)
 
     def obtener_seguidores(self):
         """
@@ -29,7 +37,7 @@ class Usuario:
         """
         # El usuario debe ya estar añadido a el archivo de seguidores
         # Leo el archivo y lo guardo en una variable
-        with open(PATH_SEGUIDORES, "r") as archivo_seguidores:
+        with open(path_seguidores, "r") as archivo_seguidores:
             listado_seguidores = archivo_seguidores.readlines()
         # Leo la lista de seguidores
         for fila_seguidores in listado_seguidores:
@@ -50,7 +58,7 @@ class Usuario:
         """
         seguidos = list()
         # Guardo el archivo en una lista
-        with open(PATH_SEGUIDORES, "r") as archivo_seguidores:
+        with open(path_seguidores, "r") as archivo_seguidores:
             listado_seguidores = archivo_seguidores.readlines()
         # itero por cada fila para buscar los usuarios seguidos
         for fila_seguidores in listado_seguidores:
@@ -63,53 +71,67 @@ class Usuario:
     def seguir_a_usuario(self, otro):
         """
         Añade al usuario (self) a la lista de seguidores de otro
+        otro es un objeto de clase Usuario
         Retorna un string, el cual es un mensaje
         que indica si se pudo seguir al usuario,
         en el caso que no se pudo se retorna el porque
         """
-        if self.nombre == otro:
+        if self.nombre == otro.nombre:
             return "No puedes seguirte a ti mismo"
         # Leo el archivo y lo guardo en una variable,
         # al iguall que en obtener_seguidores()
-        with open(PATH_SEGUIDORES, "r") as archivo_seguidores:
+        with open(path_seguidores, "r") as archivo_seguidores:
             listado_seguidores = archivo_seguidores.readlines()
         for fila_seguidores in listado_seguidores:
             fila_seguidores.strip()  # Elimino \n de las filas
             usuario, _, seguidores = fila_seguidores.strip().partition(",")
-            if usuario == otro:
+            if usuario == otro.nombre:
                 # ahora veo si ya sigue al usuario
                 if self.nombre in seguidores:
-                    return "Ya sigues a este usuario!"
+                    return f"Ya sigues a @{otro.nombre}"
                 # Si no es el caso, añado el usuario a los seguidoes
                 ubicación_cambio = listado_seguidores.index(fila_seguidores)
                 original = listado_seguidores[ubicación_cambio].strip()
                 nuevo = original + "," + self.nombre + "\n"
                 listado_seguidores[ubicación_cambio] = nuevo
                 # Ahora, reescribo el archivo original
-                with open(PATH_SEGUIDORES, "w") as archivo_seguidores:
+                with open(path_seguidores, "w") as archivo_seguidores:
                     a_guardar = "".join(listado_seguidores)
                     archivo_seguidores.write(a_guardar)
-                return f"Ahora sigues a @{otro}"
+                return f"Ahora sigues a @{otro.nombre}"
         # Si no es encontrado no se edita el archivo de seguidores
-        return f"Usuario @{otro} no encontrado"
+        return f"Usuario @{otro.nombre} no encontrado"
 
     def dejar_de_seguir(self, otro):
         """
-        Elimina al usuario (self) de la lista de seguidoresl de otro
+        Elimina al usuario (self) de la lista de seguidores del otro
+        otro es un objeto de clase Usuario
         Retorna un string, el cual es un mensaje
         que indica si se pudo parar de seguir al usuario,
         en el caso que no se pudo se retorna el porque
         """
+        # Primero veo si se sigue al otro usuario
+        if otro.nombre not in self.obtener_seguidos():
+            return f"No sigues a @{otro.nombre}"
+        # Ya que el otro si es seguido, busco su
+        # lista de seguidores y la edito
+        with open(path_seguidores, "r") as archivo_seguidores:
+            listado_seguidores = archivo_seguidores.readlines()
+        for fila_seguidores in listado_seguidores:
+            usuario, _, seguidores = fila_seguidores.strip().partition(",")
+            if usuario == otro.nombre:
+                # Edito el archivo y lo guardo
+                ubicación_cambio = listado_seguidores.index(fila_seguidores)
+                original = listado_seguidores[ubicación_cambio].strip()
+                nuevo = original.replace(f",{self.nombre}") + "\n"
+                listado_seguidores[ubicación_cambio] = nuevo
+                with open(path_seguidores, "w") as archivo_seguidores:
+                    a_guardar = "".join(listado_seguidores)
+                    archivo_seguidores.write(a_guardar)
+                return f"Paraste de seguir a @{otro.nombre}"
 
-    def crear_post(self):
-        pass
 
 
-def new_user(name):
-    # El nombre es validao anteriormente
-    lista_usuarios
-
-
-x = Usuario("m0quezada")
-print(x.obtener_seguidores())
-print(x.obtener_seguidos())
+# Leo los usuarios en usuarios.csv
+with open(path_usuarios, "r") as archivo_usuarios:
+    usuarios = {Usuario(n.strip()) for n in archivo_usuarios.readlines()}
