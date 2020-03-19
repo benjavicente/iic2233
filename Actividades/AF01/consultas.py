8c2027c7e1c3b1ab748ba3b8fa988dc716e51b55
Benjamín Vicente <benjavicente@uc.cl>
2020-03-19 15:57:16 -0300

    Primera y segunda función implementada

--- Actividades/AF01/consultas.py
+++ Actividades/AF01/consultas.py
@@ -2,12 +2,23 @@ from collections import defaultdict
 
 
 def cantidad_animes_genero(animes):
-    pass
+    cantidad_genero = defaultdict(int)
+    for anime in animes:
+        for generos in anime.Generos:
+            cantidad_genero[generos] += 1
+    return cantidad_genero
 
 
 def generos_distintos(anime, animes):
-    pass
+    gereros_anime = anime.Generos
+    set_generos = set()
+    for ani in animes:
+        set_generos = set_generos | ani.Generos
+    return set_generos - gereros_anime
 
 
 def promedio_rating_genero(animes):
+    # los 
+    dict_generos = defaultdict(int)
     pass
+
