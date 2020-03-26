aa113b8ea88b759138c310fd7c0317c3fc71b1d7
Benjamín Vicente <benjavicente@uc.cl>
2020-03-26 15:27:08 -0300

    Cambio en atributos ocultos

--- Actividades/AS01/estudiantes.py
+++ Actividades/AS01/estudiantes.py
@@ -15,34 +15,34 @@ class Estudiante:
     # Debes rellenar las property felicidad
     @property
     def felicidad(self):
-        return self.__felicidad
+        return self._felicidad
 
     @felicidad.setter
     def felicidad(self, nueva_felicidad):
         if nueva_felicidad < self.rango_felicidad[0]:
             # El valor es menor al premitido
-            self.__felicidad = self.rango_felicidad[0]
+            self._felicidad = self.rango_felicidad[0]
         elif nueva_felicidad > self.rango_felicidad[1]:
             # El valor es mayor al permitido
-            self.__felicidad = self.rango_felicidad[1]
+            self._felicidad = self.rango_felicidad[1]
         # Valor en el rango
-        self.__felicidad = nueva_felicidad
+        self._felicidad = nueva_felicidad
 
     # Debes rellenar las property estres
     @property
     def estres(self):
-        return self.__estres
+        return self._estres
 
     @estres.setter
     def estres(self, nuevo_estres):
         if nuevo_estres < self.rango_estres[0]:
             # El valor es menor al premitido
-            self.__estres = self.rango_estres[0]
+            self._estres = self.rango_estres[0]
         elif nuevo_estres > self.rango_estres[1]:
             # El valor es mayor al permitido
-            self.__estres = self.rango_estres[1]
+            self._estres = self.rango_estres[1]
         # Valor en el rango
-        self.__estres = nuevo_estres
+        self._estres = nuevo_estres
 
 
 ######## REVISAR LOS PARAMETROS
@@ -53,15 +53,15 @@ class Alumno(Estudiante):
         # Borrar pass cuando lo tengas listo
         super().__init__(username, hobbies, deberes)
         self.rango_felicidad = (0, 200)
-        self.rango_estres = (0,100) 
-        self.__felicidad = 75
-        self.__estres = 25
+        self.rango_estres = (0, 100)
+        self._felicidad = 75
+        self._estres = 25
 
     def realizar_actividad(self, actividad):
         print(f"Realizando {actividad}")
         # Debes rellenar esto, para que se ajusten los niveles de felicidad y estres
-        self.__felicidad += actividad.felicidad * 1.5 
-        self.__estres += actividad.estres
+        self._felicidad += actividad.felicidad * 1.5
+        self._estres += actividad.estres
         # Hasta acá
         print(f"El nivel de estres del estudiante es {self.estres}\
              y el de felicidad {self.felicidad}")
@@ -76,15 +76,14 @@ class Ayudante(Estudiante):
         super().__init__(username, hobbies, deberes)
         self.rango_felicidad = (0, 100)
         self.rango_estres = (0, 200)
-        self.__felicidad = 25
-        self.__estres = 75
-        pass
+        self._felicidad = 25
+        self._estres = 75
 
     def realizar_actividad(self, actividad):
         print(f"Realizando {actividad}")
         # Debes rellenar esto, para que se ajusten los niveles de felicidad y estres
-        self.__felicidad = actividad.felicidad
-        self.__estres = actividad.estres * 2
+        self._felicidad = actividad.felicidad
+        self._estres = actividad.estres * 2
         # Hasta acá
         print(f"El nivel de estres del ayudante es {self.estres}\
              y el de felicidad {self.felicidad}")
