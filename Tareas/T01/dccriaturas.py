1350cfb055be38a355e9ff75ca246fc84d8e8eba
Benjamín Vicente <benjavicente@uc.cl>
2020-04-06 18:13:04 -0400

    DCCriaturas funcional pero no completo

--- Tareas/T01/dccriaturas.py
+++ Tareas/T01/dccriaturas.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 import random
+import parametros as PMT
 
 """
 TODO:
-- Tengo que ver como utilizar valorespredeterminados en
+- Tengo que ver como utilizar valores predeterminados en
 cada una de las clases.
+- Revisar parámetors
 """
 
 
@@ -45,7 +47,7 @@ class DCCriaturas(ABC):
         valor de puntos de salud total. Su valor no
         puede superar los puntos de salud total,
         y tiene como valor mínimo 1.
-    `prob_escape` : `float`
+    `prob_escaparse` : `float`
         Valor que indica la probabilidad base que tiene
         la criatura para escapar. Es distinto según tipo,
         algunas son mas propensas a permanecer junto a
@@ -88,49 +90,80 @@ class DCCriaturas(ABC):
         `enfermarse()`
         `caracteristica_unica()`
     """
-    def __init__(self, nombre, nivel_magico, prob_escape, prop_enfermarse,
-                 enferma, escapado, vida_max, vida_actual, nivel_hambre,
-                 dias_sin_comer, agresividad, tiempo_satisfecha):
+    def __init__(self,
+                 nombre,
+                 nivel_magico=None,
+                 prob_escaparse=None,
+                 prob_enfermarse=None,
+                 enferma=None,
+                 escapado=None,
+                 vida_max=None,
+                 vida_actual=None,
+                 nivel_hambre=None,
+                 agresividad=None,
+                 dias_sin_comer=None,
+                 tiempo_satisfecha=None,
+                 **kwargs):
+        # -------------- #
+        # Valores únicos #
+        # -------------- #
         self.nombre = str(nombre)
-        # Tipo definido por subclase --> type(self).__name__
-        # -- Valores predeterminados en distintas DCCriaturas -- #
+        self.tipo = type(self).__name__
+
+        # ------------------------------------------ #
+        # Valores predeterminados en las DCCriaturas #
+        # ------------------------------------------ #
+        if vida_actual is None:
+            vida_actual = vida_max
+        if escapado is None:
+            escapado = "False"
+        if enferma is None:
+            enferma = "False"
+        if nivel_hambre is None:
+            nivel_hambre = "satisfecha"
+        if dias_sin_comer is None:
+            dias_sin_comer = "0"
+        # Transformación de valores
+        self.vida_actual = int(vida_actual)
+        self.escapado = escapado == "True"
+        self.enferma = enferma == "True"
+        self.nivel_hambre = str(nivel_hambre)
+        self.dias_sin_comer = int(dias_sin_comer)
+
+        # ------------------------------------------------ #
+        # Valores predeterminados en distintas DCCriaturas #
+        # ------------------------------------------------ #
         self.nivel_magico = int(nivel_magico)
-        self.prob_escape = float(prob_escape)
-        self.prob_enfermarse = float(prop_enfermarse)
+        self.prob_escape = float(prob_escaparse)
+        self.prob_enfermarse = float(prob_enfermarse)
         self.vida_max = int(vida_max)
         self.tiempo_satisfecha = int(tiempo_satisfecha)
         self.agresividad = str(agresividad)
-        # -- Valores predeterminados en todas lass DCCriaturas -- #
-        # predeterminado = {
-        #     "vida_actual": self.vida_max,
-        #     "escapado": False,
-        #     "enferma": False,
-        #     "nivel_hambre": "satisfecha",
-        #     "dias_sin_comer": 0
-        # }
-        self.vida_actual = int(vida_actual)
-        self.escapado = bool(escapado)
-        self.enferma = bool(enferma)
-        self.nivel_hambre = str(nivel_hambre)
-        self.dias_ultima_comida = int(dias_sin_comer)
 
     def __str__(self):
         return self.nombre
 
-    def __repr__(self):
-        return f"{type(self).__name__} {self.nombre}: vida={self.vida_actual}"
+    def __eq__(self, value):
+        return value
 
-    @abstractmethod
-    def alimentarse(self):
+    def __repr__(self):
+        return "|".join([
+                f"{type(self).__name__} '{self.nombre}':",
+                f"nivel mágico ={self.nivel_magico}",
+                f"vida = {self.vida_actual}",
+                f"nivel hambre={self.nivel_hambre}",
+                ])
+
+    def alimentarse(self, magizoologo):
         """
-        Todas las DCCriaturas comen al momento de que su Magizoólogo
-        les da alimentos. Las DCCriaturas que estén "hambrientas" y
-        sean alimentadas pasarán a estar "satisfechas". Por otro lado,
-        a la hora de comer existe una posibilidad de que la criatura
-        ataque al Magizoólogo y pierda puntos de su energía actual.
+        Magizoólogo alimenta a la criatura.
         """
-        # Formulas en TODO
-        pass
+        valor = (PMT.ALIMENTARSE_EFECTO_HAMBRE[self.nivel_hambre]
+                 + PMT.ALIMENTARSE_EFECTO_AGRESIVIDAD[self.agresividad])
+        probabilidad_atacar = min(1, valor/100)
+        if probabilidad_atacar >= random.random():
+            daño = max(0, magizoologo.nivel_magico - self.nivel_magico)
+            magizoologo.energia_actual -= daño
 
     def escaparse(self, resp_magizoologo):
         """
@@ -139,7 +172,7 @@ class DCCriaturas(ABC):
         conseguir alimento por sus propias manos.
         """
         # Formulas en TODO
-        efecto_hambre = {"satisfecha": 0, "hambrienta": 20}
+        efecto_hambre = PMT.ESCAPARSE_EFECTO_HAMBRE
         valor = (efecto_hambre[self.nivel_hambre] - resp_magizoologo)/100
         prob = min(1, self.prob_escape + max(0, valor))
         if prob >= random.random():
@@ -180,16 +213,22 @@ class Augurey(DCCriaturas):
     le gusta volar solo cuando esta bien cuidado.
     Es poco probable que ataque a su dueño.
     """
-    def __init__(self, **kwargs):
-        # predeterminado = {
-        #     "nivel_magico": random.randint(20, 50),
-        #     "prob_escarse": 0.2,
-        #     "prob_enfermarse": 0.3,
-        #     "vida_max": random.randint(35, 45),
-        #     "tiempo_satisfecha": 3,
-        #     "nivel_agresividad": "inofensiva",
-        # }
-        super().__init__()
+    def __init__(self, nombre, **kwargs):
+        # Predeterminados #
+        if "nivel_magico" not in kwargs:
+            kwargs["nivel_magico"] = random.randint(*PMT.AUGUREY_RANGO_NIVEL_MAGICO)
+        if "prob_escaparse" not in kwargs:
+            kwargs["prob_escaparse"] = PMT.AUGUREY_PROB_ESCAPARSE
+        if "prob_enfermarse" not in kwargs:
+            kwargs["prob_enfermarse"] = PMT.AUGUREY_PROP_ENFERMARSE
+        if "vida_max" not in kwargs:
+            kwargs["vida_max"] = random.randint(*PMT.AUGUREY_RANGO_VIDA_MAXIMA)
+        if "tiempo_satisfecha" not in kwargs:
+            kwargs["tiempo_satisfecha"] = PMT.AUGUREY_TIEMPO_SATISFECHA
+        if "agresividad" not in kwargs:
+            kwargs["agresividad"] = PMT.AUGUREY_NIVEL_AGRESIVIDAD
+        # Inicia la clase
+        super().__init__(nombre, **kwargs)
 
     def alimentarse(self):
         pass
@@ -206,16 +245,27 @@ class Niffler(DCCriaturas):
     por lo que suele robar Sickles para luego guardarlos en su pelaje.
     Tiende a ser agresivo.
     """
-    def __init__(self, **kwargs):
-        # predeterminado = {
-        #     "nivel_magico": random.randint(10, 20),
-        #     "prob_escarse": 0.3,
-        #     "prob_enfermarse": 0.2,
-        #     "vida_max": random.randint(20, 30),
-        #     "tiempo_satisfecha": 2,
-        #     "nivel_agresividad": "arisca",
-        # }
-        super().__init__()
+    def __init__(self, nombre, **kwargs):
+        # Predeterminados #
+        if "nivel_magico" not in kwargs:
+            kwargs["nivel_magico"] = random.randint(*PMT.NIFFLER_RANGO_NIVEL_MAGICO)
+        if "prob_escaparse" not in kwargs:
+            kwargs["prob_escaparse"] = PMT.NIFFLER_PROB_ESCAPARSE
+        if "prob_enfermarse" not in kwargs:
+            kwargs["prob_enfermarse"] = PMT.NIFFLER_PROP_ENFERMARSE
+        if "vida_max" not in kwargs:
+            kwargs["vida_max"] = random.randint(*PMT.NIFFLER_RANGO_VIDA_MAXIMA)
+        if "tiempo_satisfecha" not in kwargs:
+            kwargs["tiempo_satisfecha"] = PMT.NIFFLER_TIEMPO_SATISFECHA
+        if "agresividad" not in kwargs:
+            kwargs["agresividad"] = PMT.NIFFLER_NIVEL_AGRESIVIDAD
+        # Atributo único
+        if "nivel_cleptomania" not in kwargs:
+            self.nivel_cleptomania = random.randint(*PMT.NIFFLER_RANGO_CLEPTOMANIA)
+        else:
+            self.nivel_cleptomania = int(kwargs["nivel_cleptomania"])
+        # Inicia la clase
+        super().__init__(nombre, **kwargs)
 
     def alimentarse(self):
         pass
@@ -230,20 +280,34 @@ class Erkling(DCCriaturas):
     Con un parecido a los elfos, suelen parecer desapercibidos,
     pero son violentos.
     """
-    def __init__(self, **kwargs):
-        # predeterminado = {
-        #     "nivel_magico": random.randint(30, 45),
-        #     "prob_escarse": 0.5,
-        #     "prob_enfermarse": 0.3,
-        #     "vida_max": random.randint(50, 60),
-        #     "tiempo_satisfecha": 2,
-        #     "nivel_agresividad": "peligrosa",
-        #     "nivel_cleptomania": random.randint(5, 10),
-        # }
-        super().__init__()
+    def __init__(self, nombre, **kwargs):
+        # Predeterminados #
+        if "nivel_magico" not in kwargs:
+            kwargs["nivel_magico"] = random.randint(*PMT.ERKLING_RANGO_NIVEL_MAGICO)
+        if "prob_escaparse" not in kwargs:
+            kwargs["prob_escaparse"] = PMT.ERKLING_PROB_ESCAPARSE
+        if "prob_enfermarse" not in kwargs:
+            kwargs["prob_enfermarse"] = PMT.ERKLING_PROP_ENFERMARSE
+        if "vida_max" not in kwargs:
+            kwargs["vida_max"] = random.randint(*PMT.ERKLING_RANGO_VIDA_MAXIMA)
+        if "tiempo_satisfecha" not in kwargs:
+            kwargs["tiempo_satisfecha"] = PMT.ERKLING_TIEMPO_SATISFECHA
+        if "agresividad" not in kwargs:
+            kwargs["agresividad"] = PMT.ERKLING_NIVEL_AGRESIVIDAD
+        # Inicia la clase
+        super().__init__(nombre, **kwargs)
 
     def alimentarse(self):
         pass
 
     def caracteristica_unica(self):
         pass
+
+
+if __name__ == "__main__":
+    x = Erkling("augerrrrey")
+    y = Niffler("augerrrrey")
+    z = Augurey("augerrrrey")
+    print(x.__repr__())
+    print(y.__repr__())
+    print(z.__repr__())
