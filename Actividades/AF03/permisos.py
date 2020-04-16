831da69aa387e7ce0b8046fe98b1e02031874413
Benjamín Vicente <benjavicente@uc.cl>
2020-04-16 14:12:41 -0400

    Levantar excepciones listo

--- Actividades/AF03/permisos.py
+++ Actividades/AF03/permisos.py
@@ -2,34 +2,25 @@
 
 def verificar_rut(rut, datos_registrados):
     if "." in rut or "-" not in rut:
-        # Levanta el error correspondiente
-        pass
-
+        raise ValueError("RUT viene con puntos o sin guión.")
     if rut in datos_registrados.keys():
         return datos_registrados[rut]
     return
 
+
 def permiso_clave_unica(rut, datos_registrados):
     if rut not in datos_registrados.keys():
-        # Levanta el error correspondiente
-        pass
+        raise KeyError("No puedes solicitar clave única. Impostor!")
 
 
 def permiso_asistencia_medica(hora):
     if not hora.isdigit():
-        # Levanta el error correspondiente
-        pass
+        raise KeyError("El formato de la hora es incorrecto.")
 
 
 def permiso_servicios_basicos(persona, solicitud, comunas_cuarentena):
-
     if ((solicitud.salida not in comunas_cuarentena) and
         (solicitud.llegada not in comunas_cuarentena)):
-        # Levanta el error correspondiente
-        pass
-
+        raise ValueError("La comuna no está en cuarentena.")
     elif solicitud.salida != persona.domicilio:
-        # Levanta el error correspondiente
-        pass
-
-
+        raise ValueError("No está saliendo desde su domicilio.")
