73146c325c2f7495e9e1bcbf81e450645fe6df66
Benjamín Vicente <benjavicente@uc.cl>
2020-05-14 15:14:48 -0400

    conectado la señal

--- Actividades/AF04/main.py
+++ Actividades/AF04/main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     a = QApplication(sys.argv)
 
     # Se instancia back-end principal
-    logica_dccuent = Logica()
+    logica_dccuent = Logica(VentanaPrincipal.senal_enviar_jugada)
 
     # Se instancia la ventana inicial
     ventana_inicial = VentanaInicial()
