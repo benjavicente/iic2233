3d6883877611c4feaf973343a7d077d87912cb34
Benjamín Vicente <benjavicente@uc.cl>
2020-05-23 21:41:58 -0400

    GameClock llega a salvar el día :tada:

---
+++ Tareas/T02/backend/clock.py
@@ -0,0 +1,122 @@
+'''
+Clase GameClock que permite manejar el tiempo del juego.
+'''
+# Es más eficiente QTimer que QThread ya que *no es un thread aparte*,
+# se ejecuta junto al main loop del programa. Además, el uso es más sencillo.
+
+from PyQt5.QtCore import QTimer
+
+
+class GameClockError(Exception):
+    '''Error mostrado en el mal manejo del reloj'''
+
+
+class GameClock(QTimer):
+    '''
+    Implementación de un QTimer con un método de pausa y continuar,
+    además de la posibilidad de añadir un número de repeticiones y
+    un evento a llamar al terminar las repeticiones, pararlo o continuarlo.
+    '''
+    def __init__(self, event=None, interval: int = 1000, rep: int = -1,
+                 final_event=None, paused_event=None, continue_event=None):
+        super().__init__()
+        # Parámetros
+        self._counter = rep
+        self._interval = interval
+        self._remaining_time = False
+        # Eventos
+        self._event = event
+        self._final_event = final_event
+        self._paused_event = paused_event
+        self._continue_event = continue_event
+        # QTimer setup
+        self.setInterval(interval)
+        self.timeout.connect(self.__call_event)
+
+    def is_paused(self) -> bool:
+        '''Verifica que si el reloj esta pausado'''
+        return not self._remaining_time
+
+    def pause_(self) -> None:
+        '''Pausa el reloj'''
+        if not self.isActive():
+            raise GameClockError('El reloj ya está pausado')
+        else:
+            self._remaining_time = self.remainingTime()
+            if self._paused_event:
+                self._paused_event()
+            self.stop()
+
+    def continue_(self) -> None:
+        '''Continua el reloj'''
+        if self.isActive():
+            raise GameClockError('El reloj ya está activo')
+        else:
+            if self._continue_event:
+                self._continue_event()
+            self.start(self._remaining_time)
+
+    def __call_event(self):
+        '''
+        En vez de llamar al evento directamente, el GameClock
+        realiza unos procesos antes para permitir la pausa, la
+        limitación de iteraciones y poder llamar a un método final.
+        '''
+        # Disminuye el contador en uno si es que el contador es mayor que 0
+        if self._counter != -1:
+            self._counter -= 1
+        # Vuelve nuevamente al tiempo normal si es que se pausó
+        if self._remaining_time:
+            self.setInterval(self._interval)
+            self._remaining_time = False
+        # Ejecuta el evento si es que existe
+        if self._event:
+            self._event()
+        # Al terminar las repeticiones ejecuta el evento final si es que existe
+        if not self._counter:
+            if self._final_event:
+                self._final_event()
+            self.stop()
+
+
+
+if __name__ == "__main__":
+    # Ejemplo del funcionamiento de GameClock
+    # ---------------------------------------
+    from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+    import sys
+
+    sys.__excepthook__ = lambda t, v, trace: print(t, v, trace, sep="\n")
+    APP = QApplication(sys.argv)
+
+    EVENT = lambda: print('Hola! Terminé un ciclo')
+    FINAL_EVENT = lambda: print('Me voy')
+    PAUSE_EVENT = lambda: print('Esperando...')
+    CONTINUE_EVENT = lambda: print('Aquí volví')
+
+    TEST = GameClock(
+        event=EVENT,
+        interval=5000,
+        rep=10,
+        final_event=FINAL_EVENT,
+        paused_event=PAUSE_EVENT,
+        continue_event=CONTINUE_EVENT
+    )
+
+    WIDGET = QWidget()
+    LAYOUT = QGridLayout()
+    WIDGET.setLayout(LAYOUT)
+
+    PAUSE_BUTTON = QPushButton('pause', WIDGET)
+    CONTINUE_BUTTON = QPushButton('continue', WIDGET)
+
+    PAUSE_BUTTON.pressed.connect(TEST.pause_)
+    CONTINUE_BUTTON.pressed.connect(TEST.continue_)
+
+    LAYOUT.addWidget(PAUSE_BUTTON)
+    LAYOUT.addWidget(CONTINUE_BUTTON)
+
+    WIDGET.show()
+    TEST.start()
+
+    sys.exit(APP.exec_())
