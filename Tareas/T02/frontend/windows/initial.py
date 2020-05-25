b37d60c96c568a3fce8168976e3d14df20c36338
Benjamín Vicente <benjavicente@uc.cl>
2020-05-24 22:12:52 -0400

    código en ingles

--- Tareas/T02/frontend/windows/initial.py
+++ Tareas/T02/frontend/windows/initial.py
@@ -21,14 +21,13 @@ class InitialWindow(QWidget):
     signal_load = pyqtSignal()
     signal_new = pyqtSignal()
 
-    def __init__(self, *args, **kwards):
-        super().__init__(*args, **kwards)
+    def __init__(self):
+        super().__init__()
         self.init_ui()
 
     def init_ui(self):
         '''Inicia la ventana'''
         self.setWindowTitle('DCCafé - Inicio')
-        self.setObjectName('VentanaInicio')
         self.setStyleSheet(INITIAL_THEME)
         # TODO: Ver lo de los íconos
         #self.setWindowIcon(QIcon(QPixmap(RUTA_LOGO)))
@@ -61,49 +60,49 @@ class InitialWindow(QWidget):
         # ---------------- #
         # Cuadro principal #
         # ---------------- #
-        self.card_principal = QFrame(self)
-        self.card_principal.setObjectName('card')
-        card_principal_layout = QVBoxLayout()
-        self.card_principal.setLayout(card_principal_layout)
-        main_layout.addWidget(self.card_principal, 2, 1)
+        self.card = QFrame(self)
+        self.card.setObjectName('card')
+        card_layout = QVBoxLayout()
+        self.card.setLayout(card_layout)
+        main_layout.addWidget(self.card, 2, 1)
         # -> Logo
         self.logo = QLabel(self)
         self.logo.setFixedSize(520, 200)
         self.logo.setPixmap(QPixmap(PATH['logo']))
         self.logo.setScaledContents(True)
         # Añadilo al cuadro
-        card_principal_layout.addWidget(self.logo)
+        card_layout.addWidget(self.logo)
 
         # -> Texto de bienvenida
         # TODO: esto podría estar en parámetros
-        texto = ('Hola! Bienvenido al DCCafé!'
+        text = ('Hola! Bienvenido al DCCafé! '
                  'No está completamente listo para su apertura aún')
-        self.message = QLabel(texto, self)
+        self.message = QLabel(text, self)
         self.message.setWordWrap(True)
         self.message.setObjectName('message')
         self.message.setAlignment(Qt.AlignCenter)
         # Añadirlo al cuadro
-        card_principal_layout.addWidget(self.message)
+        card_layout.addWidget(self.message)
         # Añadir espacio
-        card_principal_layout.addSpacing(50)
+        card_layout.addSpacing(50)
 
         # -> Botones
-        fila_botones = QHBoxLayout()
-        card_principal_layout.addLayout(fila_botones)
-        fila_botones.setSpacing(20)
-        fila_botones.setContentsMargins(30, 0, 30, 0)
+        button_layout = QHBoxLayout()
+        card_layout.addLayout(button_layout)
+        button_layout.setSpacing(20)
+        button_layout.setContentsMargins(30, 0, 30, 0)
 
         # --> Cargar partida
-        self.cargar = QPushButton('Cargar Partida', self)
-        self.cargar.setCursor(QCursor(Qt.PointingHandCursor))
-        self.cargar.clicked.connect(self.load_game)
-        fila_botones.addWidget(self.cargar)
+        self.button_load_game = QPushButton('Cargar Partida', self)
+        self.button_load_game.setCursor(QCursor(Qt.PointingHandCursor))
+        self.button_load_game.clicked.connect(self.load_game)
+        button_layout.addWidget(self.button_load_game)
 
         # --> Nueva pertida
-        self.nueva = QPushButton('Nueva Partida', self)
-        self.nueva.setCursor(QCursor(Qt.PointingHandCursor))
-        self.nueva.clicked.connect(self.new_game)
-        fila_botones.addWidget(self.nueva)
+        self.button_new_game = QPushButton('Nueva Partida', self)
+        self.button_new_game.setCursor(QCursor(Qt.PointingHandCursor))
+        self.button_new_game.clicked.connect(self.new_game)
+        button_layout.addWidget(self.button_new_game)
 
         # -------- #
         # Espacios #
