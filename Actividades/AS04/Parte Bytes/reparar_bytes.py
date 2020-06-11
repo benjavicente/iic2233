d016eeeecb20c7cc0456c3710fca0749c16a69cb
Benjamín Vicente <benjavicente@uc.cl>
2020-06-11 15:27:41 -0400

    Limpieza de Parte Bytes

--- Actividades/AS04/Parte Bytes/reparar_bytes.py
+++ Actividades/AS04/Parte Bytes/reparar_bytes.py
@@ -1,24 +1,22 @@
-def reparar_imagen(ruta):
+def reparar_usuario(ruta):
+    image = bytearray()
+    # Se lee el contenido en bytes
     with open(ruta, 'rb') as file:
         content = bytearray(file.read())
-    image = bytearray()
+    # Se separan los bytes
     for i in range(0, len(content), 32):
         inverted = content[i]
+        # Se porcesa el segmento
         new_content = content[i+1:i+17]
         if inverted == 1:
             new_content.reverse()
+        # Se concatena los segmentos
         image += new_content
-    with open(R'Parte bytes\user_info.bmp', 'wb') as file:
+    # Se escribe el archivo resultante
+    with open(R'user_info.bmp', 'wb') as file:
         file.write(image)
 
 
-
-
 if __name__ == '__main__':
-    try:
-        # Problema de VSCode, elimar el path relativo
-        reparar_imagen(R'Parte bytes\imagen_danada.xyz')
-        print("Contraseña reparada")
-    except Exception as error:
-        print(f'Error: {error}')
-        print("No has podido obtener la información del Ayudante!")  
+    reparar_usuario('imagen_danada.xyz')
+    print("Contraseña reparada, ver 'user_info.bmp'")
