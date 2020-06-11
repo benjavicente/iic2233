888c326de898da018bbce6a3a43332c1f18b7ac6
Benjamín Vicente <benjavicente@uc.cl>
2020-06-11 14:23:32 -0400

    Reparada la imagen :tada:

--- Actividades/AS04/Parte Bytes/reparar_bytes.py
+++ Actividades/AS04/Parte Bytes/reparar_bytes.py
@@ -1,13 +1,23 @@
 def reparar_imagen(ruta):
-    # Completa esta función
-    pass
+    with open(ruta, 'rb') as file:
+        content = bytearray(file.read())
+    image = bytearray()
+    for i in range(0, len(content), 32):
+        inverted = content[i]
+        new_content = content[i+1:i+17]
+        if inverted == 1:
+            new_content.reverse()
+        image += new_content
+    with open(R'Parte bytes\user_info.bmp', 'wb') as file:
+        file.write(image)
 
 
 
 
 if __name__ == '__main__':
     try:
-        reparar_imagen('imagen_danada.xyz')
+        # Problema de VSCode, elimar el path relativo
+        reparar_imagen(R'Parte bytes\imagen_danada.xyz')
         print("Contraseña reparada")
     except Exception as error:
         print(f'Error: {error}')
