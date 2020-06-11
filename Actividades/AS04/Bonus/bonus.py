503d599ae195689d5a37a3ac4d7595f728abd453
Benjamín Vicente <benjavicente@uc.cl>
2020-06-11 15:20:12 -0400

    bonus listo :tada:

--- Actividades/AS04/Bonus/bonus.py
+++ Actividades/AS04/Bonus/bonus.py
@@ -1,6 +1,8 @@
+DATA = bytearray()
 
-# Debes completar este archivo
+for i in range(1, 10):
+    with open(f'secuencia_{i}', 'rb') as file:
+        DATA += bytearray(file.read())
 
 with open('clave.py', 'wb') as archivo:
-    # Aquí puedes guardar los bytes filtrados en el nuevo archivo.
-    pass
+    archivo.write(bytearray(filter(lambda b: 10 <= b <= 195, DATA)))
