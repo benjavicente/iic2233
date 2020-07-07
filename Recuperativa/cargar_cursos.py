43843882ad15660e9b867f1ef272890703b0f38b
Benjamín Vicente <benjavicente@uc.cl>
2020-07-06 23:09:27 -0400

    cargar cursos listo parece que hay un problema en el encoding de los json...

--- Recuperativa/cargar_cursos.py
+++ Recuperativa/cargar_cursos.py
@@ -7,7 +7,8 @@ def traducir_modulos(modulos_string):
     mods = dict()
     for m in map(lambda m: m.split('#'), modulos_string.split(';')):
         if len(m) - 1:
-            mods[m[0]] = list(product(*map(lambda s: s.split(','), m[1].split(':'))))
+            prod = product(*map(lambda s: s.split(','), m[1].split(':')))
+            mods[m[0]] = [(x, int(y)) for x, y in prod]
         else:
             mods[m[0]] = list()
     return mods
@@ -16,10 +17,11 @@ def traducir_modulos(modulos_string):
 def cargar_cursos(semestre: str):
     with open(path.join('datos', semestre + '.json'), encoding='utf-8') as file:
         courses = json.load(file)
-    for course in courses:
-        course['Prerrequisitos'] = course['Prerrequisitos'].split(';')
-        for secc in co
-    return course
+    for course in courses.values():
+        course['Prerrequisitos'] = list(filter(None, course['Prerrequisitos'].split(';')))
+        for secc in course['Secciones'].values():
+            secc['Modulos'] = traducir_modulos(secc['Modulos'])
+    return courses
 
 
 if __name__ == "__main__":
@@ -29,9 +31,9 @@ if __name__ == "__main__":
     horario_ejemplo_2 = "AYU;CLAS#L,W,V:3;LAB;LIB;PRA;SUP;TAL;TER;TES"
     print(traducir_modulos(horario_ejemplo_2))
 
-    semestre = "2020-1" # Cambiar para probar
+    semestre = "2019-2" # Cambiar para probar
 
     cursos = cargar_cursos(semestre)
 
     with open("resultado_cargado.json", "wt", encoding="utf-8") as archivo:
-        json.dump(cursos, archivo, indent=4)
+        json.dump(cursos, archivo, indent=2)
